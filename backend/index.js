35f20c1eb3adef303e48ec6022415980746bb540
shehroz <shehrozamir9@gmail.com>
2025-02-19 01:15:43 +0500

    update signup route

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
-// import authroutes from "./routes/auth.route.js";
+import authroutes from "./routes/auth.route.js";
 import {connectDB} from "./src/lib/db.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-
-// app.use("/api/auth", authroutes);
+app.use(express.json());
+app.use("/api/auth", authroutes);
 
 app.listen(PORT, ()=>{
     console.log(`server is running on PORT ${PORT}`);
--- backend/src/controller/auth_control.js
+++ backend/src/controller/auth_control.js
@@ -1,5 +1,31 @@
-export const signup =  (req, res)=>{
-    res.send("signup is done");
+import User from "../models/user_model";
+import bcrypt from "bcryptjs";
+export const signup = async (req, res)=>{
+    try {
+        const {fullName, email, password} = req.body;
+        if(password<6){
+            return res.status(400).json({message: "Password must be of 6 characters"});
+        }
+
+        const user = await User.findOne({email});
+        if(user) return res.status(400).json({message: "Email already exists"});
+//hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            passwor: hashedPassword,
+        }); 
+        if (newUser) {
+            
+        } else {
+            
+        }       
+    } catch (error) {
+        
+    }
 };
 
 export const login =  (req, res)=>{
